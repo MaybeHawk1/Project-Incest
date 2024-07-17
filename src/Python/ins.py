92ebecedbea00ab98e4c95bb71299e010139257b
MaybeHawk <135535190+MaybeHawk1@users.noreply.github.com>
2024-07-17 14:51:33 +0330

    Update src

---
+++ src/Python/ins.py
@@ -0,0 +1,20 @@
+test = input("Enter The Langauge You Need Incest In:\n[1]English\n[2]Germen\n[3]Japeneese\n[4]Polish\n[5]Hindi\n\n")
+
+if test == "1":
+	print("English:\tIncest")
+elif test == "2":
+	print("Germen:\tInzest")
+elif test == "3":
+        print("Japeneese:\tKonshinsōkan")
+elif test == "4":
+        print("Polish:\tkazirodztwo")
+elif test == "5":
+        print("Hindi:\tkautumbik vyabhichaar")
+else:
+	print("Not a valid language")
+
+print("\nThank you for using this app i hope it helped!")
+
+"""
+Shoutout to @1jammer1 who coded this check his profile for more awesome batarong
+"""
