dd68e1d0bd3280a36e920ee4959bb6cae93e3d90
Rioboyva2554 <116155137+Rioboyva2554@users.noreply.github.com>
2024-09-16 20:32:17 +1000

    Update ins.py

--- src/Python/ins.py
+++ src/Python/ins.py
@@ -1,17 +1,19 @@
-test = input("Enter The Langauge You Need Incest In:\n[1]English\n[2]Germen\n[3]Japeneese\n[4]Polish\n[5]Hindi\n[6]Afrikaans\n\n")
+test = input("Enter The Langauge You Need Incest In:\n[1]English\n[2]Germen\n[3]Japeneese\n[4]Polish\n[5]Hindi\n[6]Afrikaans\n[7]Latin\n\n")
 
 if test == "1":
 	print("English:\tIncest\n")
 elif test == "2":
-	print("Germen:\tInzest\n")
+	print("German:\tInzest\n")
 elif test == "3":
-        print("Japeneese:\tKonshinsōkan\n")
+        print("Japenese:\tKonshinsōkan\n")
 elif test == "4":
         print("Polish:\tkazirodztwo\n")
 elif test == "5":
         print("Hindi:\tkautumbik vyabhichaar\n")
 elif test == "6":
 	print("Afrikaans:\tbloedskande\n")
+elif test == "6":
+	print("Latin:\tincestus\n")
 else:
 	print("Not a valid language")
 
