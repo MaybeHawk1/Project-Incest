69e088929880acfdd0c0ad423211259408cb8404
Rioboyva2554 <116155137+Rioboyva2554@users.noreply.github.com>
2024-09-23 17:31:59 +1000

    Add files via upload

---
+++ src/incest.nim
@@ -0,0 +1,20 @@
+echo "Enter The Langauge You Need Incest In:\n[1]English\n[2]German\n[3]Japenese\n[4]Polish\n[5]Hindi\n[6]Afrikaans\n[7]latin\n\n"
+let choice = readLine(stdin)
+case choice
+of "1":
+  echo "English:incest"
+of "2":
+  echo "German:Inzest"
+of "3":
+  echo "Japanese:Konshinsōkan"
+of "4":
+  echo "Polish:kazirodztwo"
+of "5":
+  echo "Hindi:kautumbik vyabhichaar"
+of "6":
+  echo "Afrikaans:bloedskande"
+of "7":
+  echo "Latin:incestus"
+else:
+  echo "That's not a real Language"
+
